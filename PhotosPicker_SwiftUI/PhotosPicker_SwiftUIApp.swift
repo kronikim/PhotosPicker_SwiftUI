f0dd0ac292127cdf4fe3d5bec03ba847d1caff68
deniz karahan <denizkarahan88@gmail.com>
2022-10-04 23:02:10 +0200

    Initial Commit

---
+++ PhotosPicker_SwiftUI/PhotosPicker_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosPicker_SwiftUIApp.swift
+//  PhotosPicker_SwiftUI
+//
+//  Created by deniz karahan on 4.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct PhotosPicker_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
