ff408bba5c7ef2939a86dcebbb103c10d97a6f1d
deniz karahan <denizkarahan88@gmail.com>
2022-10-04 23:43:39 +0200

    using PhotosPicker from PhotosUI

--- PhotosPicker_SwiftUI/ContentView.swift
+++ PhotosPicker_SwiftUI/ContentView.swift
@@ -6,16 +6,42 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    @State var selectedImages : [PhotosPickerItem] = []
+    @State var data : Data?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if let data = data {
+               if let selectedImage = UIImage(data:  data) {
+                Spacer()
+
+                Image(uiImage: selectedImage)
+                .resizable()
+                .frame(width: UIScreen.main.bounds.width * 0.8 ,height: UIScreen.main.bounds.height * 0.3, alignment: .center)
+                                 
+                }
+            }
+            Spacer()
+            PhotosPicker(selection: $selectedImages, maxSelectionCount: 1,  matching: .images){
+                Text("Select Image from Photo Library")
+            }.onChange(of: selectedImages, perform: { newValue in
+                guard let image = selectedImages.first else { return }
+                image.loadTransferable(type: Data.self) { result in
+                    switch result {
+                    case .success(let data):
+                        if let data = data{
+                            self.data = data
+                        }
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
+            })
+            .padding()
         }
-        .padding()
     }
 }
 
